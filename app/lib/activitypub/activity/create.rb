17c0c9c384d2b3a77bb67ac176e74dd47005a53a
noellabo <noel.yoshiba@gmail.com>
2024-02-16 16:14:23 +0900

    Add reject pattern to Admin setting
    Conflicts:
    	app/models/form/admin_settings.rb
    	app/validators/regexp_syntax_validator.rb
    	app/views/admin/settings/shared/_links.html.haml
    	config/locales/en.yml
    	config/routes/admin.rb
    	config/settings.yml

--- app/lib/activitypub/activity/create.rb
+++ app/lib/activitypub/activity/create.rb
@@ -41,10 +41,6 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
     )
   end
 
-  def reject_pattern?
-    Setting.reject_pattern.present? && @object['content']&.match?(Setting.reject_pattern)
-  end
-
   def create_status
     return reject_payload! if unsupported_object_type? || non_matching_uri_hosts?(@account.uri, object_uri) || tombstone_exists? || !related_to_local_activity? || reject_pattern?
 
@@ -498,6 +494,10 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
 
   def tombstone_exists?
     Tombstone.exists?(uri: object_uri)
+  end
+
+  def reject_pattern?
+    Setting.reject_pattern.present? && @object['content']&.match?(Setting.reject_pattern)
   end
 
   def forward_for_reply
