fc53f9b462847fe9f54ea92a58969039d910d70a
丈槍由紀 <chikorita157@ateliershiori.moe>
2024-08-18 18:36:48 -0400

    Modify not signed in message.

--- app/javascript/flavours/glitch/features/ui/components/sign_in_banner.jsx
+++ app/javascript/flavours/glitch/features/ui/components/sign_in_banner.jsx
@@ -45,8 +45,7 @@ const SignInBanner = () => {
 
   return (
   <div className='sign-in-banner'>
-    <p><strong><FormattedMessage id='sign_in_banner.mastodon_is' defaultMessage="Mastodon is the best way to keep up with what's happening." /></strong></p>
-    <p><FormattedMessage id='sign_in_banner.follow_anyone' defaultMessage='Follow anyone across the fediverse and see it all in chronological order. No algorithms, ads, or clickbait in sight.' /></p>
+    <p><b>Sakurajima</a> is your gateway to the Japanese media (Anime/Manga/Visual Novels/Video Games/etc) fandom and creator community on the Fediverse. You can connect with other fans and creators on the Fediverse. You can learn more about our services and more on <a href='https://joinsakurajima.org'>our official website</a>.</p>
     {signupButton}
     <a href='/auth/sign_in' className='button button--block button-tertiary'><FormattedMessage id='sign_in_banner.sign_in' defaultMessage='Login' /></a>
     <p>If you signed up for an account have not received a verification email, check your SPAM folder. If you do not get it in an hour, send an email on the about page to the owner to have your account manually approved.</p>
