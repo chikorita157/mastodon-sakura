7516adaefd0e89214d08e8f31fbec0bd977172f9
丈槍由紀 <chikorita157@ateliershiori.moe>
2024-10-10 19:37:48 -0400

    Remove noreferrer
    see https://anderegg.ca/2024/05/19/whats-up-with-mastodon-and-referer-headers
    
    see https://anderegg.ca/2024/05/19/whats-up-with-mastodon-and-referer-headers

--- app/javascript/mastodon/features/status/components/detailed_status.tsx
+++ app/javascript/mastodon/features/status/components/detailed_status.tsx
@@ -239,7 +239,7 @@ export const DetailedStatus: React.FC<{
           className='detailed-status__application'
           href={status.getIn(['application', 'website'])}
           target='_blank'
-          rel='noopener noreferrer'
+          rel='noopener'
         >
           {status.getIn(['application', 'name'])}
         </a>
@@ -364,7 +364,7 @@ export const DetailedStatus: React.FC<{
               className='detailed-status__datetime'
               href={`/@${status.getIn(['account', 'acct'])}/${status.get('id')}`}
               target='_blank'
-              rel='noopener noreferrer'
+              rel='noopener'
             >
               <FormattedDate
                 value={new Date(status.get('created_at') as string)}
