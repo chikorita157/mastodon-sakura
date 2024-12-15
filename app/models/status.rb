b71e4945ce19038a746c846b621ca100f7504736
Essem <smswessem@gmail.com>
2024-12-14 21:06:08 -0600

    Allow configuring media attachment limits via environment variables

--- app/models/status.rb
+++ app/models/status.rb
@@ -47,8 +47,8 @@ class Status < ApplicationRecord
   include Status::Visibility
   include Status::InteractionPolicyConcern
 
-  MEDIA_ATTACHMENTS_LIMIT = 4
-  REMOTE_MEDIA_ATTACHMENTS_LIMIT = 16
+  MEDIA_ATTACHMENTS_LIMIT = (ENV['MAX_MEDIA_ATTACHMENTS'] || 4).to_i
+  REMOTE_MEDIA_ATTACHMENTS_LIMIT = (ENV['MAX_REMOTE_MEDIA_ATTACHMENTS'] || 16).to_i
 
   rate_limit by: :account, family: :statuses
 
